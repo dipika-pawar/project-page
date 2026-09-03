2923e97d014b25445996d4d522e91d6ec0ccd38d
mentorebrochures-sys <mentorebrochures@gmail.com>
2026-09-03 15:59:49 +0530

    update routes and fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import ProjectPage from './components/project-page';
 import ChemicalERP from './components/chemicalERP';
 import NexusIndia from './components/nexusIndia';
@@ -9,24 +10,64 @@ import CAJNK from './components/cajnk';
 import CivikExperiences from './components/civikExperiences';
 import LPC from './components/lpc';
 import Vaachi from './components/vaachi';
-import Marigold from './components/marigold'; 
+import Marigold from './components/marigold';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<ProjectPage />} />
-          <Route path="/featured-projects/chemical-erp" element={<ChemicalERP />} />
-          <Route path="/featured-projects/nexus-india" element={<NexusIndia />} />
-          <Route path="/featured-projects/beyond-six-senses" element={<BeyondSixSenses />} />
-          <Route path="/featured-projects/smart-tailoring" element={<SmartTailoring />} />
 
-          <Route path="/projects/cajnk" element={<CAJNK />} />
-          <Route path="/projects/civik-experiences" element={<CivikExperiences />} />
-          <Route path="/projects/lpc" element={<LPC />} />
-          <Route path="/projects/vaachi" element={<Vaachi />} />
-          <Route path="/projects/marigold" element={<Marigold />} />
+          {/* Home / Project Page */}
+          <Route path="/" element={<ProjectPage />} />
+
+          {/* Featured Projects */}
+          <Route
+            path="/featured-projects/chemical-erp"
+            element={<ChemicalERP />}
+          />
+
+          <Route
+            path="/featured-projects/nexus-india"
+            element={<NexusIndia />}
+          />
+
+          <Route
+            path="/featured-projects/beyond-six-senses"
+            element={<BeyondSixSenses />}
+          />
+
+          <Route
+            path="/featured-projects/smart-tailoring"
+            element={<SmartTailoring />}
+          />
+
+          {/* Projects */}
+          <Route
+            path="/projects/cajnk"
+            element={<CAJNK />}
+          />
+
+          <Route
+            path="/projects/civik-experiences"
+            element={<CivikExperiences />}
+          />
+
+          <Route
+            path="/projects/lpc"
+            element={<LPC />}
+          />
+
+          <Route
+            path="/projects/vaachi"
+            element={<Vaachi />}
+          />
+
+          <Route
+            path="/projects/marigold"
+            element={<Marigold />}
+          />
+
         </Routes>
       </div>
     </BrowserRouter>
